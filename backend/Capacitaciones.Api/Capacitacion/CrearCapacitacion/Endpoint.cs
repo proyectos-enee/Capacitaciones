bb82f11c53fc9874a8eee1d37d761f76f35cf6a9
Manuel Flores <Manuel10Flores>
2025-01-07 07:44:06 -0600

    Codigo se genera automaticamente desde el backend

--- backend/Capacitaciones.Api/Capacitacion/CrearCapacitacion/Endpoint.cs
+++ backend/Capacitaciones.Api/Capacitacion/CrearCapacitacion/Endpoint.cs
@@ -11,27 +11,35 @@ public static class Endpoint
         app.MapPost("/", async (CapacitacionRequest request, IDispatcher dispatcher) =>
             {
                 var id = Guid.NewGuid();
+                var codigoCapacitacion = GenerarCodigoCapacitacion();
+
                 var result = await dispatcher.Dispatch(new CrearCapacitacionCommand(
                     id,
-                    CodigoCapacitacion:request.CodigoCapacitacion,
-                    NombreCorto:request.NombreCorto,
-                    NombreLargo:request.NombreLargo,
-                    Descripcion:request.Descripcion,
-                    EnteCapacitador:request.EnteCapacitador,
-                    Modalidad:request.Modalidad,
-                    Lugar:request.Lugar,
-                    Horario:request.Horario,
-                    FechaInicioRegistro:request.FechaInicioRegistro,
-                    FechaFinRegistro:request.FechaFinRegistro,
-                    Estado:request.Estado
-
+                    CodigoCapacitacion: codigoCapacitacion,
+                    NombreCorto: request.NombreCorto,
+                    NombreLargo: request.NombreLargo,
+                    Descripcion: request.Descripcion,
+                    EnteCapacitador: request.EnteCapacitador,
+                    Modalidad: request.Modalidad,
+                    Lugar: request.Lugar,
+                    Horario: request.Horario,
+                    FechaInicioRegistro: request.FechaInicioRegistro,
+                    FechaFinRegistro: request.FechaFinRegistro,
+                    Estado: request.Estado
                 ));
-                return result.ToResponse(new CapacitacionResponse(){ Id = id });
+
+                return result.ToResponse(new CapacitacionResponse() { Id = id, CodigoCapacitacion = codigoCapacitacion });
             })
             .Produces<CapacitacionResponse>()
             // .RequireAuthorization()
             .WithSummary("Crear capacitacion")
             .WithDescription("Permite crear una capacitacion y devuelve el identificador de la capacitacion creada")
             .WithOpenApi();
+    }
+
+    private static string GenerarCodigoCapacitacion()
+    {
+        // código único
+        return $"CAP-{DateTime.UtcNow:yyyyMMddHHmmss}-{Guid.NewGuid().ToString("N").Substring(0, 6).ToUpper()}";
     }
 }
